71e81ed4afb71d2ca9d45c0ff5ca4d74295420f6
Elliot Chang <elliotchang126@gmail.com>
2023-10-04 13:46:20 -0700

    working on calculating points

--- backend/app.jsx
+++ backend/app.jsx
@@ -4,14 +4,27 @@ const uuid = require('uuid/v4');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 const receipts = {};
 
 const calculatePoints = (receipt) => {
+    let points = 0;
 
+    const total = parseFloat(receipt.total);
+    
+    const alphaNumChars = receipt.retailer.match(/[a-zA-Z0-9]/g);
+    points += alphaNumChars.length;
+
+    if (total % 1 === 0) points += 50;
+
+    if (total % 0.25 === 0) points += 25;
+
+    const totalItems = receipt.items.length;
+
+    points += Math.floor(totalItems / 2) * 5;
 }
 
 app.post(`/receipts`, (req, res) => {
